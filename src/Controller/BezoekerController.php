3868c7ad9d69d0196e88288663886c325a71a819
markieboy223 <markvanderburg01@gmail.com>
2022-02-09 13:29:15 +0100

    les 2

--- src/Controller/BezoekerController.php
+++ src/Controller/BezoekerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +16,34 @@ class BezoekerController extends AbstractController
     {
         return $this->render('bezoeker/index.html.twig', [
             'controller_name' => 'BezoekerController',
+        ]);
+    }
+
+    /**
+     * @Route("/categories", name="categories")
+     */
+    public function categoriesAction()
+    {
+        $categories=$this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+
+        return $this->render('bezoeker/categories.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * @Route("/category/{id}", name="category")
+     */
+    public function categoryAction($id)
+    {
+        $category=$this->getDoctrine()
+            ->getRepository(Category::class)
+            ->find($id);
+
+        return $this->render('bezoeker/category.html.twig', [
+            'category' => $category,
         ]);
     }
 }
